df6735922af436207f4d9fa2ab30a2b3c483ee34
Diana Gao <47991207+DianaGao@users.noreply.github.com>
2019-04-15 13:15:43 -0400

    Update RandomForestModel.py
    Added the visualization of the confusion matrix and CAP curve.

--- RandomForestModel.py
+++ RandomForestModel.py
@@ -34,6 +34,74 @@ y_pred = model.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
+#Visualizaing the confusion matrix
+from sklearn.utils.multiclass import unique_labels
+def plot_confusion_matrix(y_test, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(y_test, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plot_confusion_matrix(y_test, y_pred, classes = y_test,
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plot_confusion_matrix(y_test, y_pred, classes = y_test, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
+
+
 #visulizing the training result
 from matplotlib.colors import ListedColormap
 x_set, y_set = X_train, y_train
@@ -71,4 +139,55 @@ plt.ylabel("Estimated Salary")
 plt.legend()
 plt.show()
 
-
+#Visualization of the CAP curve
+import matplotlib.pyplot as plt
+from scipy import integrate
+def capcurve(y_values, y_preds_proba):
+    num_pos_obs = np.sum(y_values)
+    num_count = len(y_values)
+    rate_pos_obs = float(num_pos_obs) / float(num_count)
+    ideal = pd.DataFrame({'x':[0,rate_pos_obs,1],'y':[0,1,1]})
+    xx = np.arange(num_count) / float(num_count - 1)
+    y_cap = np.c_[y_values,y_preds_proba]
+    y_cap_df_s = pd.DataFrame(data=y_cap)
+    y_cap_df_s = y_cap_df_s.sort_values([1], ascending=False).reset_index(level = y_cap_df_s.index.names, drop=True)
+    print(y_cap_df_s.head(20))
+    
+    yy = np.cumsum(y_cap_df_s[0]) / float(num_pos_obs)
+    yy = np.append([0], yy[0:num_count-1]) #add the first curve point (0,0) : for xx=0 we have yy=0
+    
+    percent = 0.5
+    row_index = int(np.trunc(num_count * percent))
+    
+    val_y1 = yy[row_index]
+    val_y2 = yy[row_index+1]
+    if val_y1 == val_y2:
+        val = val_y1*1.0
+    else:
+        val_x1 = xx[row_index]
+        val_x2 = xx[row_index+1]
+        val = val_y1 + ((val_x2 - percent)/(val_x2 - val_x1))*(val_y2 - val_y1) 
+        
+    sigma_ideal = 1 * xx[num_pos_obs - 1 ] / 2 + (xx[num_count - 1] - xx[num_pos_obs]) * 1
+    sigma_model = integrate.simps(yy,xx)
+    sigma_random = integrate.simps(xx,xx)
+    
+    ar_value = (sigma_model - sigma_random) / (sigma_ideal - sigma_random)
+    
+    fig, ax = plt.subplots(nrows = 1, ncols = 1)
+    ax.plot(ideal['x'],ideal['y'], color='grey', label='Crystal Ball')
+    ax.plot(xx,yy, color='red', label='Customer Model')
+    #ax.scatter(xx,yy, color='red')
+    ax.plot(xx,xx, color='blue', label='Random Selection')
+    ax.plot([percent, percent], [0.0, val], color='green', linestyle='--', linewidth=1)
+    ax.plot([0, percent], [val, val], color='green', linestyle='--', linewidth=1, label=str(val*100)+'% of positive obs at '+str(percent*100)+'%')
+    plt.xlim(0, 1.02)
+    plt.ylim(0, 1.25)
+    plt.title("CAP Curve - a_r value ="+str(round(ar_value,2)))
+    plt.xlabel('% of the data')
+    plt.ylabel('% of positive obs')
+    plt.legend()
+    plt.show()
+    
+y_pred_proba = model.predict_proba(X=X_test)
+capcurve(y_values=y_test, y_preds_proba=y_pred_proba[:,1])
